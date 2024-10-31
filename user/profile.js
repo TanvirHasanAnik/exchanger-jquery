de9c6f3f4c2490cfcf1728d0cc9ed7553463833b
Tanvir Hassan Anik <tanvir.h.anik@gmail.com>
2024-10-31 16:32:34 +0600

    redirect to other user profile

--- user/profile.js
+++ user/profile.js
@@ -1,15 +1,25 @@
 
 $(document).ready(function () {
-    axios
-      .get("http://localhost:3000/user/get-profile", {
-        withCredentials: true,
-      })
-      .then(function (user) {
-        const userData = user.data;
-        console.log(userData);
-        $("#username").append("Username: "+userData.username);
-        $("#address").append("address: "+userData.address);
-        $("#email").append("email: "+userData.email);
-        $("#phone").append("phone: "+userData.phone);
-      });
+
+    function getQueryParam(param){
+      var urlParam = new URLSearchParams(window.location.search);
+      return urlParam.get(param);
+    }
+
+    var userId = getQueryParam("userId");
+    if(userId) {
+      axios
+        .get(`http://localhost:3000/user/get-profile?userid=${userId}`, {
+          withCredentials: true,
+        })
+        .then(function (user) {
+          const userData = user.data;
+          console.log(userId);
+          console.log(userData);
+          $("#username").append("Username: "+userData.username);
+          $("#address").append("address: "+userData.address);
+          $("#email").append("email: "+userData.email);
+          $("#phone").append("phone: "+userData.phone);
+        });
+    }
   });
