29d3c6135996850d00ac5d5b532ad56e21bd34be
Tanvir Hassan Anik <tanvir.h.anik@gmail.com>
2024-11-02 18:54:58 +0600

    ...

--- user/profile.js
+++ user/profile.js
@@ -22,35 +22,38 @@ $(document).ready(function () {
         });
         
         //product list
-        axios
-          .get(`http://localhost:3000/user-products/products-list?userid=${userId}`, {
-            withCredentials: true,
-          })
-          .then(function (response) {
-            const products = response.data;
-            console.log(products);
-            products.forEach((product) => {
-              const item =
-                $(`
-            <li class = "mb-1" style = "width:100%">
-              <div class="card" style="display: flex;flex-direction: column;overflow: hidden;">
-                <div class="row g-0 ">
-                  <div class="col-md-4">
-                    <img style="height:100%; object-fit: cover;" src="../product-image.png" class="img-fluid rounded-start" alt="../image-placeholder.svg">
-                  </div>
-                  <div class="col-md-8">
-                    <div class="card-body">
-                      <h5 class="card-title">${product.productTitle}</h5>
-                      <p class="card-text">${product.productDescription}</p>
-                      <p class="card-text"><small style="color:green">${product.categoryname}</small></p>
+
+        function loadProducts(){
+          axios
+            .get(`http://localhost:3000/user-products/products-list?userid=${userId}`, {
+              withCredentials: true,
+            })
+            .then(function (response) {
+              const products = response.data;
+              console.log(products);
+              products.forEach((product) => {
+                const item =
+                  $(`
+              <li class = "mb-1" style = "width:100%">
+                <div class="card" style="display: flex;flex-direction: column;overflow: hidden;">
+                  <div class="row g-0 ">
+                    <div class="col-md-4">
+                      <img style="height:100%; object-fit: cover;" src="../product-image.png" class="img-fluid rounded-start" alt="../image-placeholder.svg">
+                    </div>
+                    <div class="col-md-8">
+                      <div class="card-body">
+                        <h5 class="card-title">${product.productTitle}</h5>
+                        <p class="card-text">${product.productDescription}</p>
+                        <p class="card-text"><small style="color:green">${product.categoryname}</small></p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </li>`);
-            $("#product-list").append(item);
+              </li>`);
+              $("#product-list").append(item);
+              });
             });
-          });
+        }
           
           //review list
           function loadReviews(){
@@ -86,7 +89,8 @@ $(document).ready(function () {
               });
             });
           }
-
+          
+          loadProducts();
           loadReviews();
 
           //adding/posting review
